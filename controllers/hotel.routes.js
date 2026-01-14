59feca8f6013334232faac7b50dad0c6877c5168
doodybh <doodybhr38@gmail.com>
2026-01-14 18:39:54 +0300

    update

--- controllers/hotel.routes.js
+++ controllers/hotel.routes.js
@@ -1,7 +1,34 @@
-const router = require('express').Router();
+const Hotel = require("../models/Hotel");
 
-router.get('/', (req, res) => res.render('all-hotels.ejs'));
+const router = require("express").Router();
 
+router.get("/", async (req, res) => {
+  try {
+    const allHotels = await Hotel.find();
+    res.render("all-hotels.ejs", { allHotels: allHotels });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
+router.get("/new", (req, res) => {
+  res.render("create-hotel.ejs");
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const createdHotel = await Hotel.create(req.body);
+    res.redirect("/hotels");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  const foundHotel = await Hotel.findById(req.params.id);
+  console.log(foundHotel);
+  
+  res.render('hotel-details.ejs', {foundHotel: foundHotel});
+})
 
 module.exports = router;
